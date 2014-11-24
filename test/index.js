bff069cd88ee8dac1f5d800b7eb66ea3d1442675
Ben Drucker <bvdrucker@gmail.com>
2014-11-24 08:51:59 -0500

    Rewrite tests
    * Remove sinon and sinon-as-promised
    * Test element class instead of element and $animate method calls
    * Flush deferred removal of animation class

--- test/index.js
+++ test/index.js
@@ -1,41 +1,33 @@
 'use strict';
 
 var angular = require('angular');
-var expect  = require('chai').use(require('sinon-chai')).expect;
-var sinon   = require('sinon');
-var sap     = require('sinon-as-promised');
+var expect  = require('chai').expect;
 
 describe('animate-change', function () {
 
-  var $scope, $animate, element;
+  var $scope, $animate, $timeout, element;
   beforeEach(angular.mock.module(require('../')));
-  beforeEach(angular.mock.module(function ($provide) {
-    $provide.value('$animate', ($animate = {}));
-  }));
   beforeEach(angular.mock.inject(function ($injector) {
     $compile = $injector.get('$compile');
-    $scope = $injector.get('$rootScope').$new();
-    sap($injector.get('$q'));
-    sap.setScheduler(function (fn) {
-      $scope.$evalAsync(fn);
-    });
-    $animate.addClass = sinon.stub().resolves();
-    element = $compile('<span animate-change="foo" change-class="on"></span>')($scope);
+    $scope   = $injector.get('$rootScope').$new();
+    $timeout = $injector.get('$timeout');
+    $animate = $injector.get('$animate');
+    element  = $compile('<span animate-change="foo" change-class="on"></span>')($scope);
   }));
 
   it('adds the toggles class when the model changes', function () {
     $scope.$digest();
-    sinon.stub(element, 'removeClass');
     $scope.foo = 'bar';
     $scope.$digest();
-    expect($animate.addClass).to.have.been.calledWith(element, 'on');
-    expect(element.removeClass).to.have.been.calledWith('on');
+    expect(element.hasClass('on')).to.be.true;
+    $timeout.flush();
+    expect(element.hasClass('on')).to.be.false;
   });
 
   it('does not animate the initial value', function () {
     $scope.foo = 'bar';
     $scope.$digest();
-    expect($animate.addClass).to.not.have.been.called;
+    expect(element.hasClass('on')).to.be.false;
   });
 
 });
